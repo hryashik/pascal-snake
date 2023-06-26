ab5fc3944a43e97ac22aeb30defc2410d499278a
hryashik <tsukikohaya@gmail.com>
2023-06-26 12:00:25 +0300

    realize grow snake

--- snake.pas
+++ snake.pas
@@ -5,36 +5,16 @@ type food = record
 	CurX, CurY: integer;
 	end;
 
-procedure initFirst(var f: itemptr; var l: itemptr);
-begin
-	new(f);
-	f^.CurX := ScreenWidth div 2;
-	f^.CurY := screenHeight div 2;
-	f^.dx := -1;
-	f^.dy := 0;
-	f^.next := nil;
-	l := f;
-end;
-
-procedure addItem(var f: itemptr;var l: itemptr; var tmp: itemptr);
-begin
-	new(tmp);
-	tmp^.CurX := l^.CurX - f^.dx;
-	tmp^.CurY := l^.Cury - f^.dy;
-	tmp^.next := nil;
-	l^.next := tmp;
-	l := tmp;
-end;
 
 procedure ShowScore(score: integer);
 
 begin
-	GotoXY(ScreenWidth div 2 - 5, 1);
+	GotoXY(ScreenWidth div 2 - 2, 1);
 	write('Score: ', score);
 	GotoXY(1, 1);
 end;
 
-procedure GenFood(var f: food);
+procedure GenFood(var apple: food);
 var
 	x, y, MaxTableX, MaxTableY: integer;
 begin
@@ -43,8 +23,8 @@ begin
 	MaxTableY := ScreenHeight;
 	x := random(MaxTableX - 1) + 2;
 	y := random(MaxTableY - 1) + 2;
-	f.CurX := x;
-	f.CurY := y;
+	apple.CurX := x;
+	apple.CurY := y;
 	GotoXY(x, y);
 	TextColor(Yellow);
 	write('@');
@@ -53,26 +33,25 @@ begin
 end;
 
 var
-	key, score: integer;
-	first, last: itemptr;
-	f: food;
+	key: integer;
+	score: integer = 0;
+	apple: food;
 begin
 	clrscr();
-	score := 0;
 	ShowScore(score);
-	GenFood(f);
-	initFirst(first, last);
-	DrawStar(first);
+	GenFood(apple);
+	InitSnake();
 	while true do
 	begin
 		if not KeyPressed then
-		begin
-			MoveSnake(first);
-			if (first^.CurX = f.CurX) and (first^.CurY = f.CurY) then {snake will eat food}
 			begin
+			MoveSnake(last);
+			if (first^.CurX = apple.CurX) and (first^.CurY = apple.CurY) then
+			begin
+				AddItem();
 				score := score + 1;
 				ShowScore(score);
-				GenFood(f);
+				GenFood(apple);
 			end;	
 			delay(100);
 			continue;
@@ -86,4 +65,5 @@ begin
 			27: break
 		end
 	end;
+	clrscr();
 end.
--- snakeunit.pp
+++ snakeunit.pp
@@ -3,24 +3,58 @@ unit snakeunit;
 interface
 uses crt;
 
-type 
+type 	
 	itemptr = ^item;
 	item = record
+		head: boolean; 
 		CurX, CurY, dx, dy: integer;
 		next: itemptr;
+		prev: itemptr;
 	end;
 
+var
+	first, last, tmp: itemptr;
+
+procedure InitSnake();
+procedure AddItem();
 procedure DrawStar(var s: itemptr);
 procedure HideStar(var s: itemptr);
 procedure SetDirection(var s: itemptr; x, y: integer);
-procedure MoveSnake(var s: itemptr);
+procedure MoveSnake(var l: itemptr);
 
 implementation
+
+procedure InitSnake();
+begin
+	new(first);
+	first^.CurX := ScreenWidth div 2;
+	first^.CurY := ScreenHeight div 2;
+	first^.dx := -1;
+	first^.dy := 0;
+	first^.prev := nil;
+	first^.next := nil;
+	first^.head := true;
+	last := first;
+end;
+
+procedure AddItem();
+begin
+	new(tmp);
+	tmp^.CurX := last^.CurX - last^.dx;
+	tmp^.CurY := last^.CurY - last^.dy;
+	tmp^.head := false;
+	tmp^.next := nil;
+	tmp^.prev := last;
+	last^.next := tmp;
+	last := tmp;
+end;
 
 procedure DrawStar(var s: itemptr);
 begin
 	GotoXY(s^.CurX, s^.CurY);
 	TextColor(Green);
+	if s^.head = true then
+		TextColor(Red);
 	write('*');
 	TextColor(White);
 	GotoXY(1, 1);
@@ -39,22 +73,37 @@ begin
 	s^.dy := y;
 end;
 
-procedure MoveSnake(var s: itemptr);
+procedure MoveSnake(var l: itemptr);
 begin
-	HideStar(s);
-	s^.CurX := s^.CurX + s^.dx;
-	if s^.CurX > ScreenWidth then
-		s^.CurX := 1
-	else
-	if s^.CurX < 1 then
-		s^.CurX := ScreenWidth;
-	s^.CurY := s^.CurY + s^.dy;
-	if s^.CurY > ScreenHeight then
-		s^.CurY := 1
-	else
-	if s^.CurY < 1 then
-		s^.CurY := ScreenHeight;
-	DrawStar(s);	
+	tmp := l;
+	while tmp <> nil do
+	begin
+		HideStar(tmp);
+		tmp := tmp^.prev;
+	end;
+	tmp := l;
+	while tmp <> nil do
+	begin
+		if tmp^.head = false then
+		begin
+			tmp^.CurX := tmp^.prev^.CurX;
+			tmp^.CurY := tmp^.prev^.CurY;
+			DrawStar(tmp);
+			tmp := tmp^.prev;
+		end;
+		if tmp^.head = true then
+		begin
+			tmp^.CurX := tmp^.CurX + tmp^.dx;
+			if tmp^.CurX > ScreenWidth then
+				tmp^.CurX := 1;
+			if tmp^.CurX < 1 then
+				tmp^.CurX := ScreenWidth;
+			tmp^.CurY := tmp^.CurY + tmp^.dy;
+			DrawStar(tmp);
+			tmp := tmp^.prev;
+		end;
+	end;	
 end;
 
 end.
+
